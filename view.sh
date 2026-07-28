7b794ad8132fd82caf3d9d467b9f57cb598de449
Mauvadao4g <mauvadao4g@gmail.com>
2026-07-28 09:13:02 -0300

    280726_09:13\n\n\n========================== CHECK_V2RAY Ver: 3 Data: 2026-07-28 09:13:01 Update: New Update ==========================

--- view.sh
+++ view.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-curl -s -x "socks5h://127.0.0.1:1080" --max-time 3 https://api.ipify.org
+ip_externo=$(curl -s --max-time 3 https://api.ipify.org)
+ip_vpn=$(curl -s -x "socks5h://127.0.0.1:1080" --max-time 3 https://api.ipify.org)
+
+echo "Externo: $ip_externo"
+echo "Vpn: $ip_vpn"
