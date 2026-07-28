247e674546ddcfd16ef798c921d31e36b8d2e6a6
Mauvadao4g <mauvadao4g@gmail.com>
2026-07-28 11:25:48 -0300

    280726_11:25\numont.sh\n\n========================== CHECK_V2RAY Ver: 6 Data: 2026-07-28 11:25:48 Update: umont.sh ==========================

---
+++ umont.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+
+CONFIG='new.json'
+# jq . $CONFIG
+
+U="0265c394-17ab-4130-85a6-63ff76a5c4e0" # v2rUserId
+H="meu.host.com" # v2rHttpHost
+S="www.tim.com.br" # v2rTlsSni
+P=443
+
+
+
+UUI="${1:-$U}" # v2rUserId
+HOST="${2:-$H}" # v2rHttpHost
+SNI="${3:-$S}" # v2rTlsSni
+PORTA=${4:-$P}
+
+
+
+jq \
+  --arg uuid "$UUID" \
+  --arg host "$HOST" \
+  --arg sni "$SNI" \
+  --argjson port "$PORTA" \
+'
+.v2rUserId   = $uuid |
+.v2rHttpHost = $host |
+.v2rTlsSni   = $sni |
+.v2rPort     = $port
+' new.json
